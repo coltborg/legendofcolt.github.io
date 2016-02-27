f84c5de3190b67c60c22040f3a8d5b6048247557
Colt <colton@fariswheel.com>
2016-02-27 16:30:31 -0600

    Added kudos please buttons to single posts

--- js/rwd.js
+++ js/rwd.js
@@ -4,74 +4,9 @@
 
 jQuery(document).ready( function($){
 
-  /* Touch friendly expanded nav
-  **********************************************/
-  $('ul.navmenu li span').click( function(event) {
-
-    event.preventDefault();
-
-    // get the child of the clicked menu switch
-    var child_menu = $(this).parent().parent().children('ul');
-
-    // get the parent link of the clicked menu switch
-    var parent_link = $(this).parent();
-
-    // check if it's currently open or closed
-    if ( child_menu.hasClass('childopen') ) {
-      // if it's open, close it
-
-      // remove any "active" class from parent item
-      $(parent_link).removeClass('active');
-      // hide child menu
-      $(child_menu).removeClass('childopen');
-      // set this open menu switch to +
-      $(this).html('+');
-
-    } else {
-      // if it's closed, open it
-
-      // hide any open child menus
-      $('ul.navmenu ul').removeClass('childopen');
-      // set any open menu switch symbols back to +
-      $('ul.navmenu li span').html('+');
-      // remove any existing "active" class from menus
-      $('.active').removeClass('active');
-
-      // add "active" classto this parent item
-      $(parent_link).addClass('active');
-      // sow correct child menu
-      $(child_menu).addClass('childopen');
-      // set this open menu switch to -
-      $(this).html('-');
-
-      return false;
-
-    }
-
-  });
-
-  $('.navicon').click( function() {
-
-    console.log(event);
-
-    if ( $('.navmenu').css('display') == 'none' ) {
-
-      $('.navmenu').addClass('show');
-      $('.navicon').removeClass('closed').addClass('open');
-      $('.navicon').children('.fa').removeClass('fa-navicon').addClass('fa-close');
-
-    } else {
-
-      $('.navmenu').removeClass('show');
-      $('.navicon').removeClass('open').addClass('closed');
-      $('.navicon').children('.fa').removeClass('fa-close').addClass('fa-navicon');
-
-    }
-
-  });
-
   /* Responsive iframes
-  **********************************************/
+  ------------------------------ */
+
   function responsiveIframe() {
     $('iframe').each( function(){
       var iw = $(this).width();
@@ -104,5 +39,19 @@ jQuery(document).ready( function($){
       responsiveIframe();
     }
   }, 200);
+
+  /* Init KudosPlease
+  ------------------------------ */
+
+  new KudosPlease({
+    el : '.kudos',
+    duration : 1500,
+    persistent : true,
+    status : {
+      alpha: '',
+      beta: '',
+      gamma: ''
+    }
+  });
 
 });
